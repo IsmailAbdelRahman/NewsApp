682f7de8af751729c07b5cf9e22f3292c9aac393
Ismail AbdelRahman <conquer.spacetime@gmail.com>
2025-12-22 18:36:11 +0200

    update component null

--- lib/core/widget/Component.dart
+++ lib/core/widget/Component.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:newsappnew/core/utils/app_assets.dart';
@@ -10,8 +9,6 @@ Widget coustemCategre(
     required String urlNetImage,
     required String data,
     context}) {
-  String photo =
-      'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi8V8DdR7owOaOymCYTCrYh8fm9uceB5ETuoslBxZsQ-JehnW7mPSq37cACOZcq0ZpHWtXdrglG9j31QUfCMd9LFWBPRCm_44iQBHFex2VMFByuO6KRfwUzUraIQSI2Ti8At1DDCSi_glEQW-LGjXtN9mA7bJ1Evx9cMHYoUfP7F-2k_WnEwVsTgXVQ/s1170/%D8%A7%D8%AC%D9%85%D9%84-%D8%B5%D9%88%D8%B1-%D9%84%D9%88%D9%81%D9%8A.jpg';
   return Padding(
     padding: const EdgeInsets.all(2),
     child: Card(
